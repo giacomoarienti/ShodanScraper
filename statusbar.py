91b8320574183cb097e359793e15bc5ef98fff35
Mrjack7 <46496289+mrjack7@users.noreply.github.com>
2021-06-13 14:00:26 +0200

    Add files via upload

--- statusbar.py
+++ statusbar.py
@@ -1,7 +1,7 @@
 from sys import stdout
 
 class StatusBar(object):
-	def __init__(self, toolbar_width=40, total=0) -> None:
+	def __init__(self, total:int, toolbar_width=40) -> None:
 		self.toolbar_width = toolbar_width
 		
 		self.total = total
@@ -9,21 +9,13 @@ class StatusBar(object):
 		self.percentage = 0
 		self.bar_progress = 0
 
-	def complete(self) -> None:
-		stdout.write('\r')
-		stdout.write("Progress: [" +  ("=" * self.toolbar_width) + "] 100% ")
-		stdout.flush()
-
 	def draw(self) -> None:
 		stdout.write('\r')
 		stdout.write("Progress: [" +  ("=" * self.bar_progress) + ">" + (" " * (self.toolbar_width - self.bar_progress)) + "] " + str(self.percentage) + "%")
 		stdout.flush()
 		
-	def update(self, bar_progress) -> None:
+	def update(self, bar_progress: int) -> None:
 		self.percentage = int(round(bar_progress / self.total, 2) * 100)
-
-		if(self.percentage >= 100):
-			self.complete()
 		
-		self.bar_progress = int(self.toolbar_width * self.percentage / 100) - 1
+		self.bar_progress = int(self.toolbar_width * self.percentage / 100)
 		self.draw()
